c754ab6a9564fdbabc793a3749c6557ae81198df
DevinDiehl <devindiehl331@gmail.com>
2023-11-20 01:55:49 -0500

    first actual commit

---
+++ app/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    def testReturnBackwardsString(self):
+        string = "this is my test string"
+        reversedString = "gnirts tset ym si siht"
+        self.assertEqual(reversedString, returnBackwardsString(string))
+
+
+if __name__ == "__main__":
+    unittest.main()
